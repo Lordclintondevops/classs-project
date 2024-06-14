a1652f2fe83ce537080cff9052e8701638750f50
Lord Clinton Devops/Cloud Engineer <139438345+Lordclintondevops@users.noreply.github.com>
2024-06-14 02:38:31 -0700

    Update endpoints.tf

--- endpoints.tf
+++ endpoints.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc_endpoint" "s3_endpoint" {
     count = length(var.region)
     vpc_id = aws_vpc.liengevpc[count.index].id
-    service_name = "com.amzonaws.${var.region[0]}.s3"
+    service_name = "com.amzonaws.us-east-1.s3"
     vpc_endpoint_type = "Interface"
 
     security_group_ids = [aws_security_group.sg[count.index].id]
