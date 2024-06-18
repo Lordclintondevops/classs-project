6fca15a1ba8eeeb39a3073194cc5973f7b6c2a69
Lordclintondevops <139438345+Lordclintondevops@users.noreply.github.com>
2024-06-17 17:00:02 -0700

    added the backend.tf file

---
+++ backend.tf
@@ -0,0 +1,18 @@
+# Configuring the Backend
+
+terraform {
+  backend "s3" {
+    bucket = "lienge-group"
+    key    = "key/terraform.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "liengedb"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.53.0"
+    }
+
+  }
+}
--- main.tf
+++ main.tf
@@ -1,22 +1,4 @@
-# Configuring the Backend
-
-terraform {
-  backend "s3" {
-    bucket = "lienge-group"
-    key    = "key/terraform.tfstate"
-    region = "us-east-1"
-    # dynamodb_table = "liengedb"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.53.0"
-    }
-
-  }
-}
-
+# COnfiguring the Provider
 
 provider "aws" {
   region = var.region[0]
